7746292707ff958de907fcad3418898d6f7d191b
Olman Monge <olman1000@gmail.com>
2026-03-08 22:56:45 +0000

    actualizacion configuracion

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,25 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '', // ¡Deja vacío en desarrollo!
+
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
+
+  server: {
+  host: '0.0.0.0', // escucha todas las interfaces internas
+  port: 5173,
+  strictPort: true,
+  hmr: {
+    protocol: 'ws',
+    host: '0.0.0.0', // ✅ importante para acceder desde fuera
+    port: 5173,
+    path: '/@vite/client',
+  },
+},
 });
